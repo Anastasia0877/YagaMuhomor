1de008c8b71cb3b8f54cb97e9a028fe4aeb60fdd
Anastasia0877 <anastasiavandysheva58@gmail.com>
2023-01-16 06:07:58 +0100

    heder,footer,two block

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+$('.menu-icon').on('click', function(){
+  $('.menu-icon, .menu').toggleClass("active");
+  $('body').toggleClass("overlay");
+})
+
+$(document).click(function(e) {
+  if (!$(e.target).hasClass("active") &&
+      $(e.target).parents(".site-nav").length === 0) {
+        $('.menu-icon, .menu').removeClass("active");
+        $('body').removeClass("overlay");
+  }
+});
+$('.menu-item').on('click', function(){
+  $('.menu-icon, .menu').removeClass("active");
+  $('body').removeClass("overlay");
+})  
