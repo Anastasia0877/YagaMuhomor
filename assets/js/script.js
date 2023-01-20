732f0a5fd58d68b612c6eaf111371fddd1b42f6e
Anastasia0877 <anastasiavandysheva58@gmail.com>
2023-01-20 02:11:25 +0100

    faq, books, video, delivery NOT adaptive

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,60 @@ $('.menu-item').on('click', function(){
   $('.menu-icon, .menu').removeClass("active");
   $('body').removeClass("overlay");
 })  
+
+// accordion
+$(function() {
+	
+  //BEGIN
+  $(".accordion__title").on("click", function(e) {
+
+    e.preventDefault();
+    var $this = $(this);
+
+    if (!$this.hasClass("accordion-active")) {
+      $(".accordion__content").slideUp(400);
+      $(".accordion__title").removeClass("accordion-active");
+      $('.accordion__arrow').removeClass('accordion__rotate');
+    }
+
+    $this.toggleClass("accordion-active");
+    $this.next().slideToggle();
+    $('.accordion__arrow',this).toggleClass('accordion__rotate');
+  });
+  //END
+  
+});
+
+// delivery-slider
+// $(document).ready(function(){
+//   $('.delivery-sleder').slick({
+//     dots:true,
+//     arrows:true,
+//     slidesToShow: 3,
+//     centerMode: true,
+//     centerPadding:"0",
+//   slidesToScroll: 1,
+//   appendArrows:".delivery-slider__arrow"
+  // responsive: [
+  //   {
+  //     breakpoint: 1120,
+  //     settings: {
+  //       arrows: true,
+  //       centerMode: true,
+  //       centerPadding: '30px',
+  //       slidesToShow: 1
+  //     }
+  //   },
+  //     {
+  //     breakpoint: 1024,
+  //     settings: {
+  //       arrows: false,
+  //       centerMode: true,
+  //       centerPadding: '30px',
+  //       slidesToShow: 1,
+  //       dots:true
+  //     }
+  //   }
+  // ]
+//   });
+// });
